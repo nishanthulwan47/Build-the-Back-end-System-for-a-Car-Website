21676261c49322ca564178c9cd76c027004ed654
Nishant Hulwan <nishanthulwan47@gmail.com>
2021-01-23 12:08:47 -0500

    test cases failed for pricing-service

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,47 +1,20 @@
 package com.udacity.pricing;
-
-import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
+@SpringBootTest
+
 public class PricingServiceApplicationTests {
 
-	@LocalServerPort
-	private int port;
-
-	private String baseUrl = "http://localhost";
-
-	@Autowired
-	private TestRestTemplate testRestTemplate;
-
 	@Test
-	public void getAllCars() {
-		ResponseEntity<JSONObject> responseEntity = this.testRestTemplate.getForEntity(baseUrl + port + "/prices",
-				JSONObject.class);
-		assertThat(responseEntity.getStatusCode(), equalTo(HttpStatus.OK));
+	public void contextLoads() {
 	}
-
-	@Test
-	public void getPriceById() {
-		ResponseEntity<JSONObject> responseEntity = this.testRestTemplate.getForEntity(baseUrl + port + "/prices/1",
-				JSONObject.class);
-		assertThat(responseEntity.getStatusCode(), equalTo(HttpStatus.OK));
-	}
-
 
 
 }
+
+
---
+++ pricing-service/src/test/java/com/udacity/pricing/PricingTest.java
@@ -0,0 +1,51 @@
+package com.udacity.pricing;
+
+import com.udacity.pricing.api.PricingController;
+import com.udacity.pricing.service.PricingService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(PricingController.class)
+public class PricingTest {
+
+    public static int VALID_ID = 19;
+
+    public static int INVALID_ID = 20;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    PricingService pricingService;
+
+    @Test
+    public void getPriceWithValidId() throws Exception {
+        this.mockMvc.perform(
+                get(new URI("/services/price?vehicleId=" + VALID_ID))
+                        .contentType(String.valueOf(StandardCharsets.UTF_8))
+                        .accept(String.valueOf(StandardCharsets.UTF_8)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getPriceWithInvalidId() throws Exception {
+        this.mockMvc.perform(
+                get(new URI("/services/price?vehicleId=" + VALID_ID))
+                        .contentType(String.valueOf(StandardCharsets.UTF_8))
+                        .accept(String.valueOf(StandardCharsets.UTF_8)))
+                .andExpect(status().isNotFound());
+
+    }
+}
